09bed32d50033d9ec2b97e02da009143e32dc91a
Kushal Mudel <kushalmudel01@gmail.com>
2025-06-02 23:38:29 +0545

    learned about locators and extracting dynamic alert text

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -13,7 +13,13 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /* Maximum time expect() should wait for the condition to be met. */
+    timeout: 5000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,21 +29,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
@@ -78,4 +84,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- tests/basic-test.spec.js
+++ tests/basic-test.spec.js
@@ -1,10 +1,10 @@
-import { expect, test } from '@playwright/test';
- 
-/* 
-* JavaScript is an asynchronous programming language.
-* It means codes are not executed sequentially, it will try to execute all the codes at the same time.
-* So that we need to use Await to wait that line of code until it gets executed. 
-*/
+import { expect, test } from "@playwright/test";
+
+/*
+ * JavaScript is an asynchronous programming language.
+ * It means codes are not executed sequentially, it will try to execute all the codes at the same time.
+ * So that we need to use Await to wait that line of code until it gets executed.
+ */
 
 // {browser} is global fixture in Playwright
 
@@ -12,24 +12,40 @@ import { expect, test } from '@playwright/test';
 
 // context.newPage() is used to create a new page in the browser context
 
-/* 
-* {page} fixture can be used and it will automatically create a new browser context and a new page in that context
-* no need to use browser.newContext() and context.newPage() separately
-*/
+/*
+ * {page} fixture can be used and it will automatically create a new browser context and a new page in that context
+ * no need to use browser.newContext() and context.newPage() separately
+ */
 
-test('first playwright test', async ({ browser, page }) => {
-   
-    // const context = await browser.newContext();
-    // const page = await context.newPage();
-    
-    // Navigate to the Playwright website
-    // await page.goto('https://playwright.dev/');
+test("first playwright test", async ({ browser, page }) => {
+  // const context = await browser.newContext();
+  // const page = await context.newPage();
 
-    // Navigate to Google
-    await page.goto('https://www.google.com/');
-    console.log('Page title:', await page.title());
-    
-    // Check if the page title is correct
-    await expect(page).toHaveTitle(/Google/);
-    
-    });
+  // Navigate to the Playwright website
+  // await page.goto('https://playwright.dev/');
+
+  // Navigate to Google
+  // await page.goto('https://www.google.com/');
+  // console.log('Page title:', await page.title());
+
+  // Check if the page title is correct
+  // await expect(page).toHaveTitle(/Google/);
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  // from now onwards playwright will support fill method, type method is deprecated
+
+  // Fill the username and password fields
+  await page.locator("#username").fill("rahulshettyacademys");
+  await page.locator("#password").fill("learning");
+
+  // Click the sign-in button
+  await page.locator("#signInBtn").click();
+
+  // Wait for the alert to appear
+  console.log(await page.locator("[style*='block']").textContent());
+
+  // Assert that the alert text is as expected
+  await expect(page.locator("[style*='block']")).toContainText(
+    "Incorrect username/password."
+  );
+});
