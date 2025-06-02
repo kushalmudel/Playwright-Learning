a5d0834dcf1ef3285f12346f9344a2761276cc6d
Kushal Mudel <kushalmudel01@gmail.com>
2025-06-02 21:37:25 +0545

    learned basic assertion setup and cross browser config setup

--- tests/basic-test.spec.js
+++ tests/basic-test.spec.js
@@ -1,9 +1,9 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
  
 /* 
-# JavaScript is an asynchronous programming language.
-# It means codes are not executed sequentially, it will try to execute all the codes at the same time.
-# So that we need to use Await to wait that line of code until it gets executed. 
+* JavaScript is an asynchronous programming language.
+* It means codes are not executed sequentially, it will try to execute all the codes at the same time.
+* So that we need to use Await to wait that line of code until it gets executed. 
 */
 
 // {browser} is global fixture in Playwright
@@ -13,8 +13,8 @@ import { test } from '@playwright/test';
 // context.newPage() is used to create a new page in the browser context
 
 /* 
-# {page} fixture can be used and it will automatically create a new browser context and a new page in that context
-# no need to use browser.newContext() and context.newPage() separately
+* {page} fixture can be used and it will automatically create a new browser context and a new page in that context
+* no need to use browser.newContext() and context.newPage() separately
 */
 
 test('first playwright test', async ({ browser, page }) => {
@@ -23,6 +23,13 @@ test('first playwright test', async ({ browser, page }) => {
     // const page = await context.newPage();
     
     // Navigate to the Playwright website
-    await page.goto('https://playwright.dev/');
+    // await page.goto('https://playwright.dev/');
+
+    // Navigate to Google
+    await page.goto('https://www.google.com/');
+    console.log('Page title:', await page.title());
+    
+    // Check if the page title is correct
+    await expect(page).toHaveTitle(/Google/);
     
     });
