bac5e921893622929bed8ff99ae75d5dae8a017c
Kushal Mudel <kushalmudel01@gmail.com>
2025-06-04 23:49:18 +0545

    learned to extract multiple web elements using locators

--- tests/basic-test.spec.js
+++ tests/basic-test.spec.js
@@ -18,6 +18,12 @@ import { expect, test } from "@playwright/test";
  */
 
 test("first playwright test", async ({ browser, page }) => {
+  // storing locator in a variable
+
+  const CardTitles = page.locator(".card-body a");
+  const userName = page.locator("#username");
+  const signInBtn = page.locator("#signInBtn");
+
   // const context = await browser.newContext();
   // const page = await context.newPage();
 
@@ -35,11 +41,11 @@ test("first playwright test", async ({ browser, page }) => {
   // from now onwards playwright will support fill method, type method is deprecated
 
   // Fill the username and password fields
-  await page.locator("#username").fill("rahulshettyacademys");
+  await userName.fill("rahulshettyacademys");
   await page.locator("#password").fill("learning");
 
   // Click the sign-in button
-  await page.locator("#signInBtn").click();
+  await signInBtn.click();
 
   // Wait for the alert to appear
   console.log(await page.locator("[style*='block']").textContent());
@@ -48,4 +54,34 @@ test("first playwright test", async ({ browser, page }) => {
   await expect(page.locator("[style*='block']")).toContainText(
     "Incorrect username/password."
   );
+
+  // clearing the input field and filling it and signing in again
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signInBtn.click();
+
+  // Assert that the page contains the expected text
+  await expect(
+    page.locator(
+      "body > app-root:nth-child(2) > app-shop:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > app-card-list:nth-child(2) > app-card:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)"
+    )
+  ).toContainText("iphone X");
+
+  // another locator is used to find out second card item is Samsung Note 8
+  console.log(
+    await page
+      .locator(
+        "body > app-root:nth-child(2) > app-shop:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > app-card-list:nth-child(2) > app-card:nth-child(2) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)"
+      )
+      .textContent()
+  );
+
+  // extracting all the card titles
+  // using locator.allTextContents() method
+  // it will return an array of all the text contents of the locator
+
+  const allTitles = await CardTitles.allTextContents();
+  console.log(allTitles);
+
+  await page.waitForTimeout(5000);
 });
